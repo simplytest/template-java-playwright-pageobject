98da44b16d2d372b6adfff3ab784af37a69a56d2
Maximilian Schlipf <m.schlipfsimlytest.de>
2023-10-26 11:42:56 +0200

    refactor: 💡 rename demo test class

--- src/test/java/org/example/tests/HomepageTest.java
+++ src/test/java/org/example/tests/HomepageTest.java
@@ -4,7 +4,7 @@ import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import org.example.base.BaseTest;
 import org.testng.annotations.Test;
 
-public class TestCase extends BaseTest {
+public class HomepageTest extends BaseTest {
     @Test
     public void verifyHeadingIsVisible() {
         PlaywrightAssertions.assertThat(homepage.getHeading1()).isVisible();
